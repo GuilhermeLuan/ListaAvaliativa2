2ed70dd17758c4a90ea898b71c7a3853ad695641
Guilherme Luan <guilhermeluan@pm.me>
2023-10-24 20:19:19 -0300

    feat: versão com tratamento de saida

--- Ex1.c
+++ Ex1.c
@@ -7,19 +7,30 @@ int main() {
         scanf("%d", &primeiroArray[z]);
     }
 
+    for (int i = 0; i < 10; ++i) {
+        if (i == 9){
+            printf("%d", primeiroArray[i]);
+        } else {
+            printf("%d ", primeiroArray[i]);
+        }
+
+    }
     printf("\n");
     for (int i = 0; i < 9; ++i) {
         int segundoArray[10 - i]; // Tamanho do array diminui a cada iteração
 
         for (int z = 0; z < 9 - i; ++z) {
             int soma = primeiroArray[z] + primeiroArray[z + 1];
-            printf("%d ", soma);
+            if (z == 8 - i){
+                printf("%d", soma);
+            } else{
+                printf("%d ", soma);
+            }
+
             segundoArray[z] = soma;
         }
         printf("\n");
 
-
-        // Atualize primeiroArray para o próximo ciclo
         for (int z = 0; z < 9 - i; ++z) {
             primeiroArray[z] = segundoArray[z];
         }
